d01e1e0076c0752a6bf14b3868b71002242fcbd1
Michael Johnson <m.ianjohnson@gmail.com>
2017-05-08 20:18:25 +0000

    css/sass updating and html improvements

--- js/script.js
+++ js/script.js
@@ -1,48 +1,5 @@
 $(document).ready(function(){
-	
-	var id;
-	
-	$(window).resize(function(){
-		var newWidth = $(window).width();
-		clearTimeout(id);
-		id = setTimeout(resizeComplete(newWidth), 500);
+	$('#nav-toggle').on('click', function(){
+		$(this).next('nav').slideToggle();
 	});
-	
-	function resizeComplete(newWidth)
-	{		
-		$('#mobile-nav').find('ul').hide();
-		if( newWidth < 540 )
-		{		
-			$('#mobile-nav').show();
-			$('.hdr-top').on('click','#top_nav', function(event){
-				event.stopImmediatePropagation();
-				$('#mobile-nav').find('ul').slideToggle();
-			});
-		} else { $('#mobile-nav').hide(); }
-	}
-	
-	function viewport_resize(newWidth)
-	{
-		if( newWidth < 540 )
-		{		
-			$('.hdr-top').on('click','#top_nav', function(event){
-				event.stopImmediatePropagation();
-				$('#mobile-nav').find('ul').slideToggle();
-			});
-		}
-	}
-	
-	function viewport_init()
-	{
-		var sWidth = $(window).width();
-		if( sWidth < 540 )
-		{		
-			$('.hdr-top').on('click','#top_nav', function(event){
-				event.stopImmediatePropagation();
-				$('#mobile-nav').find('ul').slideToggle();
-			});
-		}
-	}
-	
-	viewport_init();
 });
